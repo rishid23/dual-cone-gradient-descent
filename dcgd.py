0be7809b75b60b1b230a40ebfe60acc24077e996
rishi <71474438+rishicantcode@users.noreply.github.com>
2025-06-07 13:52:41 -0700

    finishing zeroing out, working on flattening

--- dcgd.py
+++ dcgd.py
@@ -1,11 +1,12 @@
 import torch
 import torch.nn as nn
 import numpy as np
+from typing import Callable, Optional, List, Tuple
 
 class DCGD:
     
 
-    def __init__(self,params,lr=1e-3,mode='center',weight_decay=0):
+    def __init__(self,params,lr=1e-3,mode='center',weight_decay=0, eps=1e-8):
         if lr < 0.0:
             raise ValueError(f"Invalid Learning Rate: {lr}")
         
@@ -17,10 +18,23 @@ class DCGD:
         self.lr = lr
         self.weight_decay = weight_decay
         self.mode = mode.lower()
+        self.eps = eps
 
 
         if self.mode not in ["projection", "average", "center"]:
             raise ValueError(f"Invalid mode: {self.mode}. Choose from 'projection', 'average', or the 'center'")
         
         self.state = {}
+    
+    def zero_grad(self):
+        for param in self.params:
+            if parap.grad is not None:
+                param.grad_zero_()
+
+    
+    def flatten_gradients(self,params: List[torch.Tensor]) -> torch.Tensor:
+        grads = []
+        for param in params:
+            if param.grad is not None:
+                grads.appen(param.grad.view(-1))
 
