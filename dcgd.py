513630aa9f137d78bb415f5d88fe9c16bf3c72c8
rishi <71474438+rishicantcode@users.noreply.github.com>
2025-06-07 17:55:13 -0700

    impl core dcgd optimizer w/ dcg logic

--- dcgd.py
+++ dcgd.py
@@ -37,4 +37,82 @@ class DCGD:
         for param in params:
             if param.grad is not None:
                 grads.appen(param.grad.view(-1))
+            else:
+                grads.append(torch.zeros_like(param).view(-1))
+        
+        return torch.cat(grads)
+
+    def unflatten_copy(self, flat_grad: torch.Tesnor, params:List[torch.Tensor]):
+        idx = 0
+        for apram in params:
+            param_numel = param.numel()
+            param.grad = flat[idx:idx+param_numel].view_as(param).clone()
+            idx += param_numel
+    
+    def comp_gradient(self, loss:torch.Tensor) -> torch.Tensor:
+        self.zero_grad()
+        loss.backward(retain_graph=True)
+
+        return self.flatten_gradients(self.params)
+    
+    def norm_gradient(self, grad: torch.Tensor) -> torch.Tensor:
+        grad_norm = torch.norm(grad)
+        if grad_norm > self.eps:
+            reutrn grad/grad_norm
+        
+        return grad
+    
+    def proj_ortha_comp(self, grad: torch.Tensor, reference: torch.Tensor) -> torch.Tensor:
+        ref_norm = torch.norm(reference)
+        if ref_norm > self.eps:
+            ref_unit = reference/ ref_norm
+            proj = torch.dot(grad, ref_unit) * ref_unit
+            return grad - projection
+        
+        return grad
+    
+    def comp_dcdir(self, grad_r: torch.Tensor, grad_b: torch.Tensor) - > torch.Tensor:
+        inner_prod = torch.dot(grad_r, grad_b)
+
+        if self.mode == 'center':
+            grad_r_norm = self.norm_gradient(grad_r)
+            grad_b_norm = self.norm_gradient(grad_b)
+
+            bisec = grad_r_norm + grad_b_norm
+            bisec_norm = torch.norm(bisec)
+
+            if bisec_norm > self.eps:
+                bisec_norm = bisec/bisec_norm
+                total_grad = grad_b + grad_r
+
+                return torch.dot(total_grad, bisec) * bisec
+        
+            else:
+                return(grad_b+grad_r)/2
+        
+        elif self.mode == 'projection':
+            if inner_prod < 0:
+                grad_r_norm = torch.norm(grad_r)
+                grad_b_norm = torch.norm(grad_b)
+
+                total_grad = grad_b+ grad_r
+
+                if grad_r_norm >  grad_b_norm:
+                    return self.proj_ortha_comp(total_grad, grad_r)
+                else:
+                    return self.proj_ortha_comp(total_grad, grad_b)
+            
+            else:
+                return grad_r + grad_b
+        
+        elif self.mode == 'average':
+
+            proj_r = self.proj_ortha_comp(grad_r,grad_b)
+            proj_b = self.proj_ortha_comp(grad_b, grad_r)
+            return (proj_b+proj_r)/2
+        
+        else:
+            raise ValueError(f"Mode not known: {self.mode}")
+    
+
 
